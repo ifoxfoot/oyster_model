5bb7f848833f4918ca613b2409c762282f3db0be
ifoxfoot <ifoxfoot@gmail.com>
2023-01-03 13:30:23 -0800

    got reproduction at interval to work

--- oyster_model.py
+++ oyster_model.py
@@ -9,10 +9,10 @@ class Oyster(mesa.Agent):
     """An agent with randomly assigned initial energy & age."""
    
     #define init values
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, age = 0):
          super().__init__(unique_id, model)
-         self.age = random.randint(0, 3649)
          self.energy = random.randint(1,10)
+         self.age = age
 
     #define what happens at each step      
     def step(self):
@@ -21,18 +21,20 @@ class Oyster(mesa.Agent):
         self.energy += random.randint(-5, 5)
        
         # Death
-        if self.energy < 0 or self.age > 3650:
+        if (self.energy < 0) or (self.age > 3650):
             self.model.grid.remove_agent(self)
             self.model.schedule.remove(self)
             living = False
 
         #reproduction
-        if living & self.energy >= 1.5 and self.age > 365:
-            for i in range(random.randint(1,6)):
+        if living & (self.age > 365) and (self.energy > 2) and self.model.step_count%50 == 0 : 
+            for i in range(3):
                 babyOyster = Oyster(
-                    self.model.next_id(), self.model, self.energy, self.age
+                    self.model.next_id(), self.model
                 )
-                self.model.grid.place_agent(babyOyster, self.pos)
+                x = self.random.randrange(self.model.grid.width)
+                y = self.random.randrange(self.model.grid.height)
+                self.model.grid.place_agent(babyOyster, (x, y))
                 self.model.schedule.add(babyOyster)
 
     
@@ -46,19 +48,20 @@ class OysterModel(mesa.Model):
         self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
         self.running = True
+        self.step_count = 0
+        self.current_id = 0
         
         # Create agents
         for i in range(self.num_agents):
-            a = Oyster(i, self)
-            self.schedule.add(a)
-            
-            # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+            age = random.randint(1, 3649)
+            oyster = Oyster(self.next_id(), self, age)
+            self.grid.place_agent(oyster, (x, y))
+            self.schedule.add(oyster)
 
     #definte step
     def step(self):
         """Advance the model by one step."""
         self.schedule.step()
-
+        self.step_count += 1
