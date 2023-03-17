34fa253f323287f68495b295cdff162da3bc7e2f
ifoxfoot <ifoxfoot@gmail.com>
2023-03-17 14:47:58 -0700

    why won't this work

--- landlab_mesa.py
+++ landlab_mesa.py
@@ -1,11 +1,11 @@
-#import librariesf
-#from rasterio.plot import show
 #import libraries
+from rasterio.plot import show as showr
 from landlab.plot import imshow
 from landlab.io import read_esri_ascii
 from pylab import show, figure
 from landlab.components import TidalFlowCalculator
 import matplotlib.pyplot as plt
+from landlab import RasterModelGrid, imshow_grid
 
 #import mesa model
 from model import *
@@ -20,44 +20,25 @@ def generate_reef_map(model):
     raster = oys_mod.space.raster_layer.get_raster("num_oysters_in_cell")
     return(raster)
 
-#fun to plot it
-# def plot_reef_map(reef_map):  
-#     show(reef_map, interpolation='nearest')
-
-# def generate_reef_map(model):
-#     reef_map = np.zeros((model.space.raster_layer.width, model.space.raster_layer.height))
-#     for cell in model.space.raster_layer.coord_iter():
-#         cell_content, x, y = cell
-#         for agent in cell_content:
-#             if type(agent) is Oyster:
-#                 reef_map[x][y] = 1
-#             else:
-#                 reef_map[x][y] = 0
-#     return reef_map
-
+#fun to plot reef map
 def plot_reef_map(reef_map):  
-    plt.imshow(reef_map.T, interpolation='nearest')
-    plt.colorbar()
-    plt.show()
-
+    showr(reef_map)
 
 #test out funs
 oys_mod.run_model(step_count=20)
 map = generate_reef_map(oys_mod)
 plot_reef_map(map)
 
-
-dem = oys_mod.space.raster_layer.get_raster("elevation")
+rmg = RasterModelGrid((oys_mod.space.raster_layer.width, oys_mod.space.raster_layer.height))
 
 #import raster
-# (dem, z) = read_esri_ascii("data/oyster_dem.asc", name = "elevation")
-# dem.at_node.keys()
-
+(dem, z) = read_esri_ascii("data/oyster_dem_buf.asc", name = "topographic__elevation")
+dem.at_node.keys()
 
 #plot raster
-# figure('Elevations from the field')  # new fig, with a name
-# imshow.imshow_grid_at_node(dem, "elevation")
-# show()
+figure('Elevations from the field')  # new fig, with a name
+imshow.imshow_grid_at_node(dem, "topographic__elevation")
+show()
 
 #store roughness values
 rough = dem.add_zeros('mannings_n', at='node')
@@ -80,11 +61,11 @@ for i in range(50):
 
 #get innudiation rate
 rate = tfc.calc_tidal_inundation_rate()
-dem = 0.5 * rate["elevation"] * period # depth in m
+dem = 0.5 * rate["topographic__elevation"] * period # depth in m
 dem.at_node.keys()
 
 #water depth ()
 figure('water depth')  # new fig, with a name
-imshow.imshow_grid_at_node(mg, 'mean_water__depth')
+imshow_grid.imshow_grid_at_node(dem, 'mean_water__depth')
 show()
 
